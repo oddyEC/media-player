c2f6d0e36c14412fb203f61e75aaaad08bbda330
oddyec <diegomarquez2008@outlook.com>
2021-04-23 15:23:41 -0500

    typescript npm

--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,27 @@
-const video = document.querySelector("video");
-const button = document.querySelector("button");
-function MediaPlayer(config) {
-  this.media = config.el;
-}
-MediaPlayer.prototype.pause = function () {
-  this.media.pause();
-};
-MediaPlayer.prototype.play = function () {
-  this.media.play();
-};
-const player = new MediaPlayer({ el: video });
-button.onclick = () => {
-  if (player.media.paused) {
-    player.play();
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()],
+});
+
+const playButton = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
+
+const muteButton = document.querySelector('#muteButton');
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute();
   } else {
-    player.pause();
+    player.mute();
   }
 };
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  });
+}
